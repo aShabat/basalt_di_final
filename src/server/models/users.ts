caf781565dcc73023f7561d91c3a8a9d3d53797a
Anton Shabat <anton.shabat@gmail.com>
2026-02-18 23:58:57 +0200

    server: improved types for getUser model function

--- src/server/models/users.ts
+++ src/server/models/users.ts
@@ -4,14 +4,18 @@ import sql from "./sql.js"
 export async function getUser(name: string) {
   const [user] = await sql.query("select * from users where name = $1", [name])
 
-  return user as User
+  return { user: { id: user.id, name: user.name } as User, hash: user.password }
 }
 
 export async function createUser(name: string, password: string) {
-  const result = await sql.query(
-    "insert into users (name, password) values ($1, $2) returning id, name, password",
-    [name, password],
-  )
+  try {
+    const [result] = (await sql.query(
+      "insert into users (name, password) values ($1, $2) returning id, name",
+      [name, password],
+    )) as User[]
 
-  return result
+    return result
+  } catch (err) {
+    return undefined
+  }
 }
