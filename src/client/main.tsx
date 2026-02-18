f388def0ce197103657d98eea0b422a2943dd38d
Anton Shabat <anton.shabat@gmail.com>
2026-02-18 23:54:50 +0200

    client: created main.css

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+import "./main.css"
+
 import App from "./App"
 import { BrowserRouter } from "react-router"
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
