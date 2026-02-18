9f90e572d2097f8aa5f5aadc7bcb82c2361e7c7a
Anton Shabat <anton.shabat@gmail.com>
2026-02-18 23:56:14 +0200

    server: add cookie middleware

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import ViteExpress from "vite-express"
+import cookieParser from "cookie-parser"
 
 import userRouter from "./routes/user.js"
 
 const app = express()
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use("/api/user", userRouter)
 
