8aaf9540ac3d03f14c1b9fac5faec24b407da493
Anton Shabat <anton.shabat@gmail.com>
2026-02-20 17:00:00 +0200

    client add UserContext and fetch user when starting an app

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -3,15 +3,33 @@ import LogIn from "./LogIn"
 import SignUp from "./SignUp"
 import Notes from "./Notes"
 import Redirect from "./Redirect"
+import { useEffect, useState } from "react"
+import UserContext from "./UserContext"
+import { User } from "../server/types"
+import { getUser } from "./api"
 
 function App() {
+  const ctx = useState(undefined as User | undefined)
+
+  useEffect(() => {
+    ;(async () => {
+      const user = await getUser()
+      if (user) {
+        const [_, setUser] = ctx
+        setUser(user)
+      }
+    })()
+  })
+
   return (
-    <Routes>
-      <Route path="notes/*" element={<Notes />} />
-      <Route path="login" element={<LogIn />} />
-      <Route path="signup" element={<SignUp />} />
-      <Route path="*" element={<Redirect path={"/notes"} />} />
-    </Routes>
+    <UserContext value={ctx}>
+      <Routes>
+        <Route path="notes/*" element={<Notes />} />
+        <Route path="login" element={<LogIn />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="*" element={<Redirect path={"/notes"} />} />
+      </Routes>
+    </UserContext>
   )
 }
 
--- src/client/api.ts
+++ src/client/api.ts
@@ -1,6 +1,16 @@
 import axios from "axios"
+import { User } from "../server/types"
 
 const axiosI = axios.create({ baseURL: "/api" })
+
+export async function getUser() {
+  const response = await axiosI.get("/user")
+  if (response.status === 200) {
+    return response.data as User
+  } else {
+    return undefined
+  }
+}
 
 export async function postUser(name: string, password: string) {
   const response = await axiosI.post("/user", { name, password })
