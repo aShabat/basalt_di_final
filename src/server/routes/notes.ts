8cb3eea4491fb79a95f9fbe8be0fb524a2557244
Anton Shabat <anton.shabat@gmail.com>
2026-02-21 11:27:10 +0200

    server streamlined notes route

--- src/server/routes/notes.ts
+++ src/server/routes/notes.ts
@@ -4,55 +4,54 @@ import { Folder, Note } from "../types.ts"
 
 const router = Router()
 
-function stripContents(f: Folder): Folder {
-  return {
-    ...f,
-    subfolders: f.subfolders.map(stripContents),
-    notes: f.notes.map((n) => ({ ...n, contents: "" })),
+interface StrippedNote extends Omit<Note, "id"> {}
+
+interface StrippedFolder extends Omit<Folder, "id" | "notes" | "subfolders"> {
+  notes: string[]
+  subfolders: StrippedFolder[]
+}
+function stripContents(item: Folder | Note): StrippedFolder | StrippedNote {
+  if (item.kind === "note") {
+    return {
+      kind: "note",
+      contents: item.contents,
+      title: item.title,
+    } as StrippedNote
+  } else {
+    return {
+      kind: "folder",
+      title: item.title,
+      subfolders: item.subfolders.map(stripContents),
+      notes: item.notes.map((n) => n.title),
+    } as StrippedFolder
   }
 }
 
-router.get("/:name/tree", async (req, res) => {
-  console.log("here")
-  const name = req.params.name
-  if (req.user === undefined || req.user.name !== name) {
-    res.sendStatus(401)
-    return
-  }
-
-  const rootFolder = await getNotes(name)
-
-  res.json(stripContents(rootFolder))
-})
-
-function folderFind(folder: Folder, path: string[]): Folder | Note {
-  if (path.length === 0) return folder
-  const [p, ...rest] = path
-  const matchingNotes = folder.notes.filter((n) => n.title === p)
-  if (matchingNotes.length === 1 && path.length === 1) return matchingNotes[1]
-  const matchingFolders = folder.subfolders.filter((f) => f.title === p)
-  if (matchingFolders.length === 1) return folderFind(matchingFolders[0], rest)
-  throw new Error("couldn't go along the path")
+function folderFind(folder: Folder, title: string): Folder | Note {
+  const matchingFolders = folder.subfolders.filter((s) => s.title === title)
+  if (matchingFolders.length === 1) return matchingFolders[0]
+  const matchingNotes = folder.notes.filter((n) => n.title === title)
+  if (matchingNotes.length === 1) return matchingNotes[0]
+  throw new Error("error")
 }
-
-router.get("/:name/note/*path", async (req, res) => {
+router.get("/:name/*path", async (req, res) => {
   const { name, path } = req.params
   if (req.user === undefined || req.user.name !== name) {
     res.sendStatus(401)
     return
   }
 
-  let rootFolder = await getNotes(name)
   try {
-    const note = folderFind(rootFolder, path)
-    if (note.type === "note") {
-      res.json(note)
-    } else {
-      res.sendStatus(404)
+    let returnItem: Folder | Note = await getNotes(name)
+    let step = 0
+    while (returnItem.kind === "folder" && step < path.length) {
+      returnItem = folderFind(returnItem, path[step])
+      step++
     }
+    if (step < path.length) throw new Error()
+
+    res.json(stripContents(returnItem))
   } catch (_) {
     res.sendStatus(404)
   }
 })
-
-export default router
