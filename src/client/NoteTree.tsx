f09ea29a8cabe3121109a10cbb4cb5ff3fceb734
Anton Shabat <anton.shabat@gmail.com>
2026-02-23 17:11:26 +0200

    client improved NoteTree

--- src/client/NoteTree.tsx
+++ src/client/NoteTree.tsx
@@ -1,16 +1,51 @@
+import { useState } from "react"
 import { ApiFolder } from "../server/types"
 
-interface Props {
+interface NoteRowProps {
+  title: string
+}
+
+function NoteRow({ title }: NoteRowProps) {
+  return <li>{title}</li>
+}
+
+interface FolderRowProps {
+  folder: ApiFolder
+}
+function FolderRow({ folder }: FolderRowProps) {
+  const [open, setOpen] = useState(false)
+  const children = folder.children.map((f) => <FolderRow folder={f} />)
+  const notes = folder.notes.map((t) => <NoteRow title={t} />)
+
+  function handleClick() {
+    setOpen(!open)
+  }
+  return (
+    <li>
+      <span onClick={handleClick}>{folder.title}</span>
+      {open ? (
+        <ul>
+          {children}
+          {notes}
+        </ul>
+      ) : (
+        <></>
+      )}
+    </li>
+  )
+}
+
+interface TreeProps {
   className?: string
   root?: ApiFolder
 }
-export default function NoteTree({ className, root }: Props) {
+export default function NoteTree({ className, root }: TreeProps) {
   return (
     <div className={className}>
       {root ? (
         <ul>
-          {root.children.map((s) => (
-            <li key={s.title}> {s.title} </li>
+          {root.children.map((f) => (
+            <FolderRow folder={f} />
           ))}
         </ul>
       ) : (
